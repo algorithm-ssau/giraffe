a7849e5e10831626362c88a5493d10372e756cbc
Anton <suhov.toxa@gmail.com>
2021-03-31 02:19:54 +0400

    navandcar1

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import {Navbar,Nav,Carousel} from "react-bootstrap";
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
 ReactDOM.render(
     <React.StrictMode>
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" fixed = "top">
             <Navbar.Brand href="#home">4ABREC-ECO</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Pizza</Nav.Link>
@@ -18,38 +17,36 @@ ReactDOM.render(
             </Nav>
         </Navbar>
         <Carousel>
-            <Carousel.Item>
+            <Carousel.Item >
                 <img
                     className="d-block w-100"
-                    src="https://s1.eda.ru/StaticContent/Photos/120131085053/171027192707/p_O.jpg"
+                    src="https://s1.1zoom.ru/b5050/434/Fast_food_Pizza_542007_1366x768.jpg"
                     alt="Pizza"
                 />
                 <Carousel.Caption>
                     <h3>Pizza</h3>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item >
                 <img
                     className="d-block w-100"
-                    src="https://s1.eda.ru/StaticContent/Photos/120131085053/171027192707/p_O.jpg"
-                    alt="Pizza"
+                    src="https://cdn.wallscloud.net/converted/1611188683-burgeri-7JNA-1366x768-MM-100.jpg"
+                    alt="Burger"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>Burger</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg"
+                    src="https://look.com.ua/pic/202011/1366x768/look.com.ua-365903.jpg"
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h3>Rolls</h3>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
