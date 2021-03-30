5e653a1c30d3a5059ec8e560b099cb38f70651d6
Anton <suhov.toxa@gmail.com>
2021-03-31 01:37:15 +0400

    navandcar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Navbar,Nav,Carousel} from "react-bootstrap";
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">4ABREC-ECO</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Pizza</Nav.Link>
+                <Nav.Link href="#features">Burgers</Nav.Link>
+                <Nav.Link href="#pricing">Rolls</Nav.Link>
+            </Nav>
+        </Navbar>,
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://s1.eda.ru/StaticContent/Photos/120131085053/171027192707/p_O.jpg"
+                    alt="Pizza"
+                />
+                <Carousel.Caption>
+                    <h3>Pizza</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://s1.eda.ru/StaticContent/Photos/120131085053/171027192707/p_O.jpg"
+                    alt="Pizza"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
   </React.StrictMode>,
   document.getElementById('root')
 );
