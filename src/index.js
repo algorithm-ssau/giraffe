b236a25ccd044aeca4884818d736781881750f97
Anton <suhov.toxa@gmail.com>
2021-03-31 00:00:09 +0400

    navbar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Navbar,Nav,Carousel} from "react-bootstrap";
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">4ABREC-ECO</Navbar.Brand>
+          <Nav className="mr-auto">
+              <Nav.Link href="#home">Pizza</Nav.Link>
+              <Nav.Link href="#features">Burgers</Nav.Link>
+              <Nav.Link href="#pricing">Rolls</Nav.Link>
+          </Nav>
+      </Navbar>,
   </React.StrictMode>,
   document.getElementById('root')
 );
