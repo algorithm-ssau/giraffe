ef1036baa8565ff166ec83fa2cc2781bedf4c19e
4abrec <63412490+4abrec@users.noreply.github.com>
2021-05-08 22:33:25 +0400

    route

---
+++ server/data/products.js
@@ -0,0 +1,87 @@
+const products = [
+    {
+        name: "Маргаритта",
+        description:
+            "Состав: Сыр моцарелла, томат черри, орегано, томатный соус. Вес 781 г",
+        price: 290,
+        category: "Пицца",
+        imageUrl: "client/public/images/marg.png",
+    },
+    {
+        name: "Ветчина и сыр",
+        description:
+            "Состав: Ветчина, сыр моцарелла, томатный соус. Вес 780 г",
+        price: 350,
+        category: "Пицца",
+        imageUrl: "client/public/images/ham.png",
+    },
+    {
+        name: "Мясная",
+        description:
+            "Состав: Куриное филе, ветчина, бекон, колбаски пепперони, сыр моцарелла, томатный соус. Вес 930 г",
+        price: 400,
+        category: "Пицца",
+        imageUrl: "client/public/images/mys.png",
+    },
+    {
+        name: "Пепперони",
+        description:
+            "Состав: Колбаски пепперони, сыр моцарелла, томатный соус. Вес 680 г",
+        price: 350,
+        category: "Пицца",
+        imageUrl: "client/public/images/peperony.png",
+
+    },
+    {
+        name: "Мексиканская",
+        description:
+            "Состав: Куриное филе, перец сладкий, томат черри, сыр моцарелла, острый перец халапеньо, томатный соус. Вес 850 г",
+        price: 380,
+        category: "Пицца",
+        imageUrl: "client/public/images/mexic.png",
+    },
+    {
+        name: "Гавайская",
+        description:
+            "Состав: Куриное филе, ананас, сыр моцарелла, соус белый. Вес 900 г",
+        price: 390,
+        category: "Пицца",
+        imageUrl: "client/public/images/gav.png",
+    },
+    {
+        name: "Филадельфия",
+        description:
+            "Состав: Лосось, огурец, авокадо, кунжут, сыр, зеленый лук, 6 шт. Вес 225 г",
+        price: 250,
+        category: "Роллы",
+        imageUrl: "client/public/images/fila.png",
+    },
+    {
+        name: "Калифорния",
+        description:
+            "Состав: Краб-микс, авокадо, огурец, тобико, 6 шт. Вес 170 г",
+        price: 250,
+        category: "Роллы",
+        imageUrl: "client/public/images/kalifornia.png",
+    },
+    {
+        name: "Гауди",
+        description:
+            "Состав: Лосось копченый, руккола, перец, сыр, авокадо, кунжут, зеленый лук, 6 шт. Вес 170 г",
+        price: 250,
+        category: "Роллы",
+        imageUrl: "client/public/images/gaudy.jpg",
+
+    },
+    {
+        name: "Курай",
+        description:
+            "Состав: Лосось, угорь, сыр, кунжут и острый соус, 6 шт. Вес 165 г",
+        price: 250,
+        category: "Роллы",
+        imageUrl: "client/public/images/kurai.jpg",
+
+    },
+
+];
+module.exports = products;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT
@@ -12,6 +14,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "API running..." });
 });
+
+app.use("/api/products", productRoutes);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
