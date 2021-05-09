bb2ad0a76e848a2415a8765af8d6d565747d39f4
4abrec <63412490+4abrec@users.noreply.github.com>
2021-05-09 21:08:42 +0400

    render

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Header from './components/header/Header';
+import Label from "./components/label/Label";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/*<App />*/}
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.Fragment>
+        <Header/>
+    </React.Fragment>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+    <React.StrictMode>
+        <Label />
+    </React.StrictMode>,
+    document.getElementById('label')
+);
+
 reportWebVitals();
