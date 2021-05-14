38a09f89a25d18d8a75fbd4b93a7c09c0f0d2274
SeregeySulimenko <sergey-sulimenko@mail.ru>
2021-05-15 01:28:02 +0400

    reviews

--- client/src/components/review/Review.js
+++ client/src/components/review/Review.js
@@ -36,25 +36,18 @@ class Review extends Component {
                             <div className="heading">
                                 <h2>Отзывы клиентов</h2>
                             </div>
-
-                            <div className="swiper-container" data-slide-effect="slide" data-autoheight="false"
-                                 data-swiper-speed="500" data-swiper-margin="25" data-swiper-slides-per-view="3"
-                                 data-swiper-breakpoints="true" data-scrollbar="true" data-swiper-loop="true"
-                                 data-swpr-responsive="[1, 2, 2, 2]">
-
-                                <div className="swiper-wrapper pb-90 pb-sm-60 left-text center-sm-text">
-                                    <div className="swiper-slide">
+                            <div className="container">
+                                <div className="row">
                                     {reviews.map(review =>
-                                        <div>
-                                            <h4>{review.heading}</h4>
-                                            <p className="color-ash mb-50 mb-sm-30 mt-20">{review.text}</p>
-                                            <h6><b className="color-primary">{review.name}</b>
-                                            </h6>
+                                        <div className="col-lg-6 col-md-6  col-sm-6 ">
+                                            <div className="center-text mb-30">
+                                                <h4>{review.heading}</h4>
+                                                <p className="color-ash mb-30 mb-sm-20 mt-10">{review.text}<br/><b
+                                                    className="color-primary">{review.name}</b></p>
+                                            </div>
                                         </div>
                                     )}
-                                    </div>
                                 </div>
-                                <div className="swiper-pagination"></div>
                             </div>
                         </div>
                     </section>
